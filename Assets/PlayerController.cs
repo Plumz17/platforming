2e6c22bbbbf285933a0bc0bedc7963882a5fe7e2
Plumz17 <121227766+Plumz17@users.noreply.github.com>
2025-03-14 22:04:06 +0700

    Version 0.1.0
    Added Jump Buffering and Clamped Fall Speed

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,3 +1,15 @@
+//Added Features:
+// Sideways Movement
+// Jumping
+// Ground Check
+// Variable Jump Height
+// Coyote Time
+// Apex Modifier
+// Jump Buffering
+// Clamped Fall Speed
+// (Edge Detection looks difficult, got no time for that)
+// Github page: https://github.com/Plumz17/platforming
+
 using System;
 using UnityEngine;
 
@@ -7,13 +19,14 @@ public class PlayerController : MonoBehaviour
     private float moveInput;
     private bool isGrounded;
     private float coyoteTimeCounter;
-
+    private float jumpBufferCounter;
 
     [Header("Movement Settings")]
     public float speed = 10f;
     public float jumpForce = 10f;
     public float lowJumpMultiplier = 1.5f; 
     public float fallMultiplier = 2f; 
+    public float maxFallSpeed = 20f; 
 
     [Header("Ground Settings")]
     public float groundCheckDistance = 1.1f;
@@ -21,74 +34,76 @@ public class PlayerController : MonoBehaviour
 
     [Header("Coyote Time Settings")]
     [SerializeField] private float coyoteTime = 0.2f;
+    [SerializeField] private float jumpBufferTime = 0.25f;
 
-    void Awake()
-    {
+
+    void Awake() {
         rb = GetComponent<Rigidbody2D>();
-    }
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-    
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
         HandleJumping();
         CheckGround();
         ApplyVariableJump();
+        Debug.Log(rb.linearVelocityY);
     }
 
-    void FixedUpdate()
-    {
+    void FixedUpdate() {
         HandleMovement();
     }
 
-    private void HandleJumping()
-    {
-        if (Input.GetKeyDown(KeyCode.Space) && coyoteTimeCounter > 0f)
-        {
+    private void HandleJumping() {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            jumpBufferCounter = jumpBufferTime;
+        }
+        else {
+            jumpBufferCounter -= Time.deltaTime;
+        }
+
+        if (jumpBufferCounter > 0f && coyoteTimeCounter > 0f) {
             Jump();
         }
     }
 
-    private void HandleMovement()
-    {
+    private void HandleMovement() {
         moveInput = Input.GetAxisRaw("Horizontal");
         rb.linearVelocity = new Vector2(moveInput * speed, rb.linearVelocityY);
     }
 
     private void CheckGround()
     {
-        if (Physics2D.Raycast(transform.position, Vector2.down, groundCheckDistance, GroundLayer))
-        {
+        if (Physics2D.Raycast(transform.position, Vector2.down, groundCheckDistance, GroundLayer)) {
+            if (!isGrounded && jumpBufferCounter > 0f) {
+                Jump(); 
+            }
             isGrounded = true;
-            coyoteTimeCounter = coyoteTime;  // Reset coyote time when grounded
+            coyoteTimeCounter = coyoteTime;  
         }
-        else
-        {
+        else {
             isGrounded = false;
-            coyoteTimeCounter -= Time.deltaTime;  // Countdown coyote time when in air
+            coyoteTimeCounter -= Time.deltaTime; 
         }
     }
 
-    void ApplyVariableJump()
-    {
-        if (rb.linearVelocityY < 0)
-        {
+    void ApplyVariableJump() {
+        if (rb.linearVelocityY < 0) {
             // Apply extra gravity when falling
             rb.linearVelocity += Vector2.up * Physics2D.gravity.y * fallMultiplier * Time.deltaTime;
         }
-        else if (rb.linearVelocityY > 0 && !Input.GetKey(KeyCode.Space))
-        {
+        else if (rb.linearVelocityY > 0 && !Input.GetKey(KeyCode.Space)) {
             // Apply lower gravity before peak if jump button is released early
             rb.linearVelocity += Vector2.up * Physics2D.gravity.y * lowJumpMultiplier * Time.deltaTime;
+        }
+
+        if (rb.linearVelocityY < -maxFallSpeed) {
+            rb.linearVelocity = new Vector2(rb.linearVelocityX, -maxFallSpeed);
         }
     }
 
     void Jump() {
         rb.linearVelocity = new Vector2(rb.linearVelocityX, jumpForce);
         coyoteTimeCounter = 0f;
+        jumpBufferCounter = 0f;
     }
 }
