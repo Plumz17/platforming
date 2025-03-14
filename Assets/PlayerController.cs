5a8a4aa3360cdec708abbb3f5ce4b495169d1d0f
Plumz17 <121227766+Plumz17@users.noreply.github.com>
2025-03-14 21:20:31 +0700

    Version 0.0.2
    Added Apex Jump Modifier + Coyote Jump

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -6,16 +6,21 @@ public class PlayerController : MonoBehaviour
     private Rigidbody2D rb;
     private float moveInput;
     private bool isGrounded;
+    private float coyoteTimeCounter;
 
 
     [Header("Movement Settings")]
     public float speed = 10f;
     public float jumpForce = 10f;
+    public float lowJumpMultiplier = 1.5f; 
+    public float fallMultiplier = 2f; 
 
     [Header("Ground Settings")]
     public float groundCheckDistance = 1.1f;
     public LayerMask GroundLayer;
 
+    [Header("Coyote Time Settings")]
+    [SerializeField] private float coyoteTime = 0.2f;
 
     void Awake()
     {
@@ -30,24 +35,60 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        moveInput = Input.GetAxisRaw("Horizontal");
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded) {
-            Jump();
-        }
+        HandleJumping();
         CheckGround();
-    }
-
-    private void CheckGround()
-    {
-        isGrounded = Physics2D.Raycast(transform.position, Vector2.down, groundCheckDistance, GroundLayer);
-    }
-
-    void Jump() {
-        rb.linearVelocity = new Vector2(rb.linearVelocityX, jumpForce);
+        ApplyVariableJump();
     }
 
     void FixedUpdate()
     {
+        HandleMovement();
+    }
+
+    private void HandleJumping()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && coyoteTimeCounter > 0f)
+        {
+            Jump();
+        }
+    }
+
+    private void HandleMovement()
+    {
+        moveInput = Input.GetAxisRaw("Horizontal");
         rb.linearVelocity = new Vector2(moveInput * speed, rb.linearVelocityY);
+    }
+
+    private void CheckGround()
+    {
+        if (Physics2D.Raycast(transform.position, Vector2.down, groundCheckDistance, GroundLayer))
+        {
+            isGrounded = true;
+            coyoteTimeCounter = coyoteTime;  // Reset coyote time when grounded
+        }
+        else
+        {
+            isGrounded = false;
+            coyoteTimeCounter -= Time.deltaTime;  // Countdown coyote time when in air
+        }
+    }
+
+    void ApplyVariableJump()
+    {
+        if (rb.linearVelocityY < 0)
+        {
+            // Apply extra gravity when falling
+            rb.linearVelocity += Vector2.up * Physics2D.gravity.y * fallMultiplier * Time.deltaTime;
+        }
+        else if (rb.linearVelocityY > 0 && !Input.GetKey(KeyCode.Space))
+        {
+            // Apply lower gravity before peak if jump button is released early
+            rb.linearVelocity += Vector2.up * Physics2D.gravity.y * lowJumpMultiplier * Time.deltaTime;
+        }
+    }
+
+    void Jump() {
+        rb.linearVelocity = new Vector2(rb.linearVelocityX, jumpForce);
+        coyoteTimeCounter = 0f;
     }
 }
